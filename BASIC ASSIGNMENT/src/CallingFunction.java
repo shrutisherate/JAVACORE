9cf3e6dcdfd75795127b62b890f6fd6738305e67
sherateshruti <sherateshruti@gmail.com>
2022-09-11 19:27:46 +0530

    first commit

---
+++ BASIC ASSIGNMENT/src/CallingFunction.java
@@ -0,0 +1,21 @@
+
+public class CallingFunction {
+	
+
+	    static void myMethod() {
+	        System.out.println("My Name is 'Shruti sherate'");
+	    }
+
+	    public static void main(String[] args) {
+	        /* To call a method, you just need to type the method name
+	           followed by open and closed parentheses */
+	        myMethod();
+	    }
+	
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
