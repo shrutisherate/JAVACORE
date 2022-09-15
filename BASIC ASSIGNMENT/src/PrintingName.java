888f94776b92e317995e643c416dc0b4976f01ee
sherateshruti <sherateshruti@gmail.com>
2022-09-15 15:21:33 +0530

    commit first

---
+++ BASIC ASSIGNMENT/src/PrintingName.java
@@ -0,0 +1,13 @@
+
+// Write a program to print your name.
+
+public class PrintingName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Shruti Sherate";
+		System.out.println("My name is " + name);
+
+	}
+
+}
