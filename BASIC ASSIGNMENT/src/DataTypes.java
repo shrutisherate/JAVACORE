d7c31008ef2dbf4086e6e2958985e695256630ee
sherateshruti <sherateshruti@gmail.com>
2022-09-15 13:59:33 +0530

    commit first

---
+++ BASIC ASSIGNMENT/src/DataTypes.java
@@ -0,0 +1,20 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1000;                // Default size is 4 byte
+        float f = 22.2f;            // Default size is 4 byte
+        double d = 33.3333;        // Default size is 8 byte
+        char c = 'S';               // Default size is 2 byte
+        boolean b = false;           // Default size is 1 byte
+
+        System.out.println("This is an Integer " + i);
+        System.out.println("This is a Float " + f);
+        System.out.println("This is a Double " + d);
+        System.out.println("This is a Char " + c);
+        System.out.println("This is a Boolean " + b);
+
+	}
+
+}
