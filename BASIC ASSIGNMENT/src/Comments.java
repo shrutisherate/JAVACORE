3af796ec5b186694c819e666fc6dbf2e0cdd8a30
sherateshruti <sherateshruti@gmail.com>
2022-09-15 13:33:53 +0530

    commit first

---
+++ BASIC ASSIGNMENT/src/Comments.java
@@ -0,0 +1,16 @@
+
+
+// Write a program for a Single line comment, multi-line and documentation comments
+
+public class Comments {
+	public static void main(String[] args) {
+		 System.out.println("// This is a Single line comment");
+
+	        System.out.println("/* This is a Multi-line comment */");
+
+	        System.out.println("/** This is a Documentation comment */");
+	
+
+	}
+
+}
