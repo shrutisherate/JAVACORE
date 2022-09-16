cee7471b67373440d7097d773cebcbdadb7f5a96
sherateshruti <sherateshruti@gmail.com>
2022-09-16 23:07:10 +0530

    commit first

---
+++ Operators/src/Relational.java
@@ -0,0 +1,17 @@
+ // Program for relational operators (<,<==, >, >==)
+public class Relational {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		        int a = 200;
+		        int b = 10;
+		        System.out.println(a < b);  //less than ( < )
+		        System.out.println(a > b);  //greater than ( > )
+		        System.out.println(a <= b); //less than or equals( <= )
+		        System.out.println(a >= b); //greater than or equals( >= )
+		    }
+		}
+
+	
+
