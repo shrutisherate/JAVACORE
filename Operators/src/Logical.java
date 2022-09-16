4b300969e498e09f89a8df267099625ac75d7577
sherateshruti <sherateshruti@gmail.com>
2022-09-16 22:58:17 +0530

    commit first

---
+++ Operators/src/Logical.java
@@ -0,0 +1,30 @@
+
+// Programs on Logical AND,OR operator and Logical NOT 
+
+public class Logical {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 40;
+        int b = 20;
+        int c = 5;
+        // Logical AND (&&)
+        /* The logical && operator doesn't check the second condition if the
+        first condition is false. It checks the second condition only if the first one is true. */
+        System.out.println(a > b && a > c); // true  && true  = true
+        System.out.println(a < b && a < c); // false && false = false
+        System.out.println(a > b && a < c); // true  && false = false
+
+        //Logical OR (||)
+        /* The logical || operator doesn't check the second condition if the
+        first condition is true.It checks the second condition only if the first one is false. */
+        System.out.println(a > b || a > c); // true  || true  = true
+        System.out.println(a < b || a < c); // false || false = false
+        System.out.println(a < b || a > c); // false || true  = true
+
+        //Logical NOT [!()]
+        System.out.println(!(a > b));      // NOT true = false
+        System.out.println(!(a < b));      // NOT false = true
+    }
+}
+	
