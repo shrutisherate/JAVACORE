4cdd76c0914df8bbdb1129df4e03e8a3226012e0
sherateshruti <sherateshruti@gmail.com>
2022-09-17 13:33:17 +0530

    commit first

---
+++ Loops Assignments/src/PrintingNumbers.java
@@ -0,0 +1,19 @@
+
+
+// Write a java program to print 1 to 20 numbers using the while loop.
+public class PrintingNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+        //In while loop we separately need to initialize and increment the variable
+        //loop executes from 1 to 20
+        while (i <= 20) {
+            System.out.print(i + " ");
+            //Every time the loop body is executed, this expression increments
+            i++;
+        }
+
+	}
+
+}
