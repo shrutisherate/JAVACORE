37827d74f4e387290876b730fbcd52618ccb27dc
sherateshruti <sherateshruti@gmail.com>
2022-09-17 14:40:14 +0530

    commit first

---
+++ Loops Assignments/src/PrintingNumbersDoWhile.java
@@ -0,0 +1,18 @@
+//  Write a program to print 1 to 10 using the do-while loop statement.
+
+public class PrintingNumbersDoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i = 1;
+		 do {
+	            System.out.print(i + " ");
+	            i++;
+	        }
+		 
+		 while (i <= 10);
+	}
+	
+	
+
+}
