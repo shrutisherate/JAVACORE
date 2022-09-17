8e19ce5c470882a8b4a4c70684c0d14a49f54d9a
sherateshruti <sherateshruti@gmail.com>
2022-09-17 14:34:51 +0530

    commit first

---
+++ Loops Assignments/src/EvenNumbers10_100.java
@@ -0,0 +1,18 @@
+
+// Write a program to print even number between 10 and 100 using while
+
+public class EvenNumbers10_100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The Even Numbers between 10 and 100 are : ");
+        int i = 10;
+        //loop executes from 10 to 100
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2;
+        }
+
+	}
+
+}
