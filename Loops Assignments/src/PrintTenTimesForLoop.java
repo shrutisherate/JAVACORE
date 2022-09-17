81e2816d510b8e8510234b6c6812d4e1e887ad37
sherateshruti <sherateshruti@gmail.com>
2022-09-17 13:20:36 +0530

    commit first

---
+++ Loops Assignments/src/PrintTenTimesForLoop.java
@@ -0,0 +1,19 @@
+
+
+
+// Write a program to print “Bright IT Career” ten times using for loop
+
+public class PrintTenTimesForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//If the number of iteration is fixed then use for loop
+        // Syntax : for(initialization ; condition ; increment/decrement)
+        //loop Executes from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + ".Bright IT Career");
+
+	}
+
+}
+}
