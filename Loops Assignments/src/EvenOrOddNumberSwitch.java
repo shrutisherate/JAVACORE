0d392be1f5701a1c2ed315109e44e6a4686c4b61
sherateshruti <sherateshruti@gmail.com>
2022-09-17 22:37:42 +0530

    commit first

---
+++ Loops Assignments/src/EvenOrOddNumberSwitch.java
@@ -0,0 +1,21 @@
+//  Program to check whether a number is EVEN or ODD using switch
+
+import java.util.Scanner;
+public class EvenOrOddNumberSwitch {
+	public static void main(String[] args) {
+        int n;
+        System.out.print("Enter a number : ");
+        //user input stored in n
+        n = new Scanner(System.in).nextInt();
+        switch (n % 2) {
+        case 0:
+            System.out.println(n + " is an Even Number");
+            break;
+        //Odd number has a remainder of 1 if divided by 2
+        case 1:
+            System.out.println(n + " is an Odd Number");
+            break;
+    }
+}
+
+}
