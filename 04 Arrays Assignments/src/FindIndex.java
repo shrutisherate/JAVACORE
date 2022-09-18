797b8de18257f0236dffe0a91255524c2ce0b8ff
sherateshruti <sherateshruti@gmail.com>
2022-09-18 18:02:26 +0530

    commmit first

---
+++ 04 Arrays Assignments/src/FindIndex.java
@@ -0,0 +1,28 @@
+// Write a program to find the index of an array element
+import java.util.*;
+public class FindIndex {
+	 static void arrIndex(int[] arr, int n) {
+	        for (int i = 0; i < arr.length; i++)
+	        	 if (n == arr[i]) {
+	                 System.out.println(n + " is at index " + i);
+	                 return;
+	        	 }
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int index;
+        //declaring and initializing an array
+        int[] arr = {11, 22, 33, 44, 55, 66, 77};
+        System.out.println("arr = " + Arrays.toString(arr));
+        //user input
+        System.out.print("Select any value to find Index of array : ");
+        Scanner sc = new Scanner(System.in);
+        index = sc.nextByte();
+        //passing array and index to method
+        arrIndex(arr, index);
+    }
+
+	}
+
+
