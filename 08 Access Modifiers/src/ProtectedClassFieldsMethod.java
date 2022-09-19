634307994f7659268645353b3168500f4b97d4c8
sherateshruti <sherateshruti@gmail.com>
2022-09-19 20:56:11 +0530

    commit first

---
+++ 08 Access Modifiers/src/ProtectedClassFieldsMethod.java
@@ -0,0 +1,27 @@
+
+/*
+ Create a class with PROTECTED fields and methods. Access these fields and methods
+ * from any other class in the same package.
+ */
+
+public class ProtectedClassFieldsMethod {
+    //Protected field
+    protected String name;
+
+    //Protected method
+    protected void protectedMethod() {
+        System.out.println("This is a " + name);
+    }
+}
+
+//Accessing protected fields and methods from other class in the same package
+class protectedClass_03 {
+    public static void main(String[] args) {
+        //Creating object of ProtectedClassFieldsMethod class.
+        ProtectedClassFieldsMethod pc = new ProtectedClassFieldsMethod();
+        //Accessing protected field
+        pc.name = "Protected Method";
+        //Accessing protected method
+        pc.protectedMethod();
+    }
+}
