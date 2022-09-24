6a36e1c81da9b0686aa65a1f6dacdeceaf0f37c4
sherateshruti <sherateshruti@gmail.com>
2022-09-24 20:10:13 +0530

    commit first

---
+++ 14 Exception Assignment/src/GenerateArithmeticException.java
@@ -0,0 +1,20 @@
+
+// 8. Write a program to generate Arithmetic Exception
+
+
+public class GenerateArithmeticException {
+
+    public static void main(String[] args) {
+        int a = 10, b = 0, c;
+        try {
+            //below code throws divide by zero exception
+            c = a / b;
+
+            //handles the Arithmetic Exception / Divide by zero exception
+        } catch (ArithmeticException e) {
+            System.err.println("ArithmeticException caught!");
+            
+            e.printStackTrace();
+        }
+    }
+}
