29cf6564c7ebd844c2a15e6c21ce138a6d8557a5
sherateshruti <sherateshruti@gmail.com>
2022-09-24 21:54:09 +0530

    commit first

---
+++ 14 Exception Assignment/src/NoSuchMethod.java
@@ -0,0 +1,29 @@
+
+// 14.Write a program to generate NoSuchMethodException
+
+import java.lang.reflect.Method;
+//creating a class
+class E_14 {
+    //creating a method which receives two integer values as parameters
+    public void addition(int a, int b) {
+        int c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
+
+public class NoSuchMethod {
+    public static void main(String[] args) {
+
+        try {
+            Class c = Class.forName("com.jala.exceptions.E_14");
+            //There is no subtraction() method in class E_14
+            Method m = c.getDeclaredMethod("subtraction", int.class, int.class);
+        }
+        //Throws NoSuchMethodException
+        catch (NoSuchMethodException | ClassNotFoundException e) {
+           
+            e.printStackTrace();
+        }
+    }
+}
